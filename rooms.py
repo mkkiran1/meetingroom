2722ea223851d2e5e383811228423d832d6cc623
Ravi Kiran <mkkiran1@gmail.com>
2020-06-12 12:32:41 -0700

    romms.py files

--- rooms.py
+++ rooms.py
@@ -1,62 +1,69 @@
-import csv
-import itertools
+"""
+This program needs to be run where we have rooms.txt is accessible.
+rooms.txt has the meeting rooms data with the rooms availability
+The input is given in this program itself for which the meeting room availability need to be checked.
+Input : For 5 team members, search for the room located on the 8th floor, with the meeting start time 10:30 and end time 11:30
+NOTE: code written with python 3.8.2 
+"""
 
-input = "5,8,10:30,11:30"
-a = input.split(",")
-mem = a[0]
-floor = a[1]
-timings_from = a[2]
-timings_till = a[3]
+from datetime import datetime
 
-def meeting_room(mem,floor,timings_from, timings_till):
-    print(timings_from + timings_till)
-    members = []
-    floors = []
-    rooms = []
-    length = []
-    count=0
-    time = []
-    allocate=False
-    diff = 0
-    last_diff = 1
-    fl=0
-    rm=0
-    with open('rooms.txt', newline='') as f:
-        reader = csv.reader(f)
-        your_list = list(reader)
-        for i in your_list:
-            timings = []
-            length.append(len(i))
-            x=i[0].split(".")
-            floors.append(x[0])
-            rooms.append(x[1])
-            members.append(i[1])
-            for j in range(2,len(i)):
-                timings.insert(count,i[j])
-                count = count+1
-            time.append(timings)
-    count=0
-    for i in members:
-        if mem > i:
-            print("Room "+rooms[count]+" with floor "+floors[count]+" not available for "+members[count])
-        else:
-            allocate=True
-        count=count+1
-    count=0
-    for (i,j,k,l) in itertools.zip_longest(floors,rooms,members,time):
-        if timings_from in l and timings_till in l:
-            if mem <= k:
-                if floor == i:
-                    print("Hello, "+i + j + timings_from + timings_till)
-                    fl=i
-                    rm=j
-                else:
-                    diff = abs(int(int(floor) - int(i)))
-                    if diff <= last_diff:
-                        last_diff = diff
-                        fl=i
-                        rm=j    
-        count=count+1
-    print(str(fl)+"."+str(rm))
-meeting_room(mem, floor, timings_from,timings_till)
+def timediff(time1, time2):
+    timea = datetime.strptime(time1, "%H:%M")
+    timeb = datetime.strptime(time2, "%H:%M")
+    if timea >= timeb:
+        newtime = timea - timeb
+        return int(newtime.seconds / 60)
+    else:
+        newtime = timea - timeb
+        return int(-newtime.seconds / 60)
 
+
+def getclosest(lst, K):
+    num = lst[min(range(len(lst)), key=lambda i: abs(lst[i] - K))]
+    return num
+
+input1 = "7,7,11:00,11:30"
+ipcapacity, ipfloor, ipstart, ipend = input1.split(",")
+#print("ipcapacity:" + ipcapacity + "\nipfloor:" + ipfloor + "\nipstart:" + ipstart + "\nipend:" + ipend)
+availablerooms = []
+
+roomsfile = open('C:\\workspace\\ConferenceRoom\\rooms.txt', 'r')
+while True:
+    # Reading next line from file
+    line = roomsfile.readline()
+
+    # if line is empty
+    # End of file is reached
+    if not line:
+        break
+    #To strip the line for all white spaces and split it with comma
+    x = line.strip().split(",")
+    #For further the first string from above split to get floor and room details
+    floor = x[0].split('.')[0]
+    room = x[0].split('.')[1]
+    capacity = x[1]
+    #To check the capacity of meeting room
+    if capacity >= ipcapacity:
+        for i in range(2, len(x), 2):
+            #check the starting time and its availability 
+            checkStarttime = timediff(ipstart, x[i])
+            if checkStarttime >= 0:
+                checkEndtime = timediff(x[i + 1], ipend)
+                # check meeting end time availability if starting time available as per the duration of meeting
+                if checkEndtime >= 0:
+                    #collect info for all available meeting room 
+                    availablerooms.append(x[0])
+
+
+roomsfile.close()
+if not availablerooms:
+    #if no room is available as per the requirement 
+    print("Sorry, no meeting available to meet your requirement")
+else:
+#   converting list of available meeting rooms into dict
+    floorrooms = {int(fr.split('.')[0]): int(fr.split('.')[1]) for fr in availablerooms}
+#   sorting the dict on floor and picking the closest floor
+#   same logic could be use to get the nearest meeting room in case needed
+    closestfloor = getclosest(sorted(floorrooms), int(ipfloor))
+    print("{0}.{1}".format(closestfloor, floorrooms[closestfloor]))
